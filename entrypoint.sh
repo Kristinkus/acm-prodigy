23526e8fdf67d01e82ed345efca6ae407a8d048d
Vladimir Velikovich <vvelikovich@gmail.com>
2025-04-01 13:44:14 +0300

    [fix] added volunteers migrations in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,12 @@
 
 python3 manage.py collectstatic --noinput --no-input --clear
 
-python3 manage.py makemigrations main
+python3 manage.py makemigrations main volunteers
 
 python3 manage.py migrate
+
+python3 manage.py init_config
+
+python3 manage.py load_translations translations.json --force
 
 exec "$@"
