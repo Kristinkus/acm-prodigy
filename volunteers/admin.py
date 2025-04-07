0b1e593bb7362a5c77639f66fe27c6869fdbbb8b
Vladimir Velikovich <vvelikovich@gmail.com>
2025-04-07 18:58:01 +0300

    [feat] added models to admin view with fields

--- volunteers/admin.py
+++ volunteers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from volunteers.models import (
     Volunteer,
     VolunteerParticipation,
@@ -7,8 +8,32 @@ from volunteers.models import (
     Event,
 )
 
-admin.site.register(Volunteer)
-admin.site.register(VolunteerParticipation)
 admin.site.register(VolunteerPositions)
 admin.site.register(Locations)
 admin.site.register(Event)
+
+@admin.register(VolunteerParticipation)
+class VolunteerParticipationAdmin(admin.ModelAdmin):
+    list_display = (
+        'volunteer',
+        'event__name',
+        'status',
+    )
+    list_filter = (
+        'status',
+        'event__name',
+    )
+
+
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'education',
+        'phone',
+        'view_telegram',
+    )
+
+    @admin.display(empty_value='---')
+    def view_telegram(self, obj):
+        return format_html(f'<a href="{obj.telegram_link}">Telegram</a>')
